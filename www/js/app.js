22b17e582a62d32d7edd3c70af57a552180f50ca
Virgilio Ximenes <virgiliorximenes@gmail.com>
2016-03-28 21:10:56 -0300

    Creating register screen

--- www/js/app.js
+++ www/js/app.js
@@ -25,23 +25,29 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
 
-  .state('initial', {
-      url: '/initial',
-      templateUrl: 'templates/initial.html'
-  })
-  
-  .state('login', {
-      url: '/login',
-      templateUrl: 'templates/login.html'
-  });
+    .state('initial', {
+        url: '/initial',
+        templateUrl: 'templates/initial.html'
+    })
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/initial');
+    .state('login', {
+        url: '/login',
+        templateUrl: 'templates/login.html'
+    })
+
+    .state('register', {
+        url: '/register',
+        templateUrl: 'templates/register.html'
+    })
+    ;
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/initial');
 
 });
